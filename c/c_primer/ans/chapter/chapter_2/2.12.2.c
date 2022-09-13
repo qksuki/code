795419698946cb57188a02e9d9cde6f4c8ff47f9
qksuki <qksuki@outlook.com>
2022-09-13 23:29:30 +0800

    test

--- c/c_primer/ans/chapter/chapter_2/2.12.2.c
+++ c/c_primer/ans/chapter/chapter_2/2.12.2.c
@@ -2,9 +2,9 @@
  * @Author: qksuki qksuki@outlook.com
  * @Date: 2022-09-13 22:31:46
  * @LastEditors: qksuki qksuki@outlook.com
- * @LastEditTime: 2022-09-13 23:03:38
+ * @LastEditTime: 2022-09-13 23:29:16
  * @FilePath: \codes\c\c_primer\ans\chapter\chapter_2\2.12.2.c
  * @Description:
- * t
+ * ts
  * Copyright (c) 2022 by qksuki qksuki@outlook.com, All Rights Reserved.
  */
