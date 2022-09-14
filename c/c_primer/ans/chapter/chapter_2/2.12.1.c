c9d994397ebbeba85d68d70c6909146f44d50d80
qksuki <qksuki@outlook.com>
2022-09-14 18:13:34 +0800

    2.12.1

--- c/c_primer/ans/chapter/chapter_2/2.12.1.c
+++ c/c_primer/ans/chapter/chapter_2/2.12.1.c
@@ -2,10 +2,13 @@
  * @Author: qksuki qksuki@outlook.com
  * @Date: 2022-09-12 23:10:23
  * @LastEditors: qksuki qksuki@outlook.com
- * @LastEditTime: 2022-09-13 22:30:45
+ * @LastEditTime: 2022-09-14 18:12:53
  * @FilePath: \codes\c\c_primer\ans\chapter\chapter_2\2.12.1.c
  * @Description:
- *
+ * 编写一个程序，调用一次 printf()函数，把你的姓名打印在一行。再调
+用一次 printf()函数，把你的姓名分别打印在两行。然后，再调用两次printf()
+函数，把你的姓名打印在一行。输出应如下所示（当然要把示例的内容换成
+你的姓名）
  * Copyright (c) 2022 by qksuki qksuki@outlook.com, All Rights Reserved.
  */
 
